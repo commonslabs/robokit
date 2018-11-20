3316de68122acf2f64c77501da4252ef4510d5bc
Thomas Countz <thomascountz@gmail.com>
2018-11-20 09:16:56 -0500

    fix typos in tests

--- transform2d_tests.py
+++ transform2d_tests.py
@@ -11,25 +11,25 @@ class TestTransform2d(unittest.TestCase):
         expected = vector
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_in_x_will_return_translate2dd_vector(self):
+    def test_translate2d_by_1_in_x_will_return_translate2d_vector(self):
         vector = np.array([0, 0])
         result = transform2d.translate2d(vector, 1, 0)
         expected = np.array([1, 0])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_in_y_will_return_translate2dd_vector(self):
+    def test_translate2d_by_1_in_y_will_return_translate2d_vector(self):
         vector = np.array([0, 0])
         result = transform2d.translate2d(vector, 0, 1)
         expected = np.array([0, 1])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_x_and_1_y_will_return_translate2dd_vector(self):
+    def test_translate2d_by_1_x_and_1_y_will_return_translate2d_vector(self):
         vector = np.array([0, 0])
         result = transform2d.translate2d(vector, 1, 1)
         expected = np.array([1, 1])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_x_and_y_will_return_translate2dd_vector(self):
+    def test_translate2d_by_x_and_y_will_return_translate2d_vector(self):
         vector = np.array([3, 1])
         result = transform2d.translate2d(vector, 5, 8)
         expected = np.array([8, 9])
@@ -49,7 +49,6 @@ class TestTransform2d(unittest.TestCase):
     def test_translate2d_with_3d_vector_will_raise_exception(self):
         vector = np.array([0, 0, 0])
         self.assertRaises(ValueError, transform2d.translate2d, vector, 1, 1)
-
 
 if __name__ == '__main__':
     unittest.main()
