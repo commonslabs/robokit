76bbab7c09fa0f411684bf8cd8b7ed6f1b0a9541
Thomas Countz <thomascountz@gmail.com>
2018-11-19 21:00:35 -0500

    add 2d translation

--- transform2d.py
+++ transform2d.py
@@ -1,7 +1,48 @@
-def foo():
-    """Return "bar"
-    >>> foo()
-    'bar'
-    """
+# -*- coding: utf-8 -*-
+"""transform2d
 
-    return "bar"
+This module contains several funtions to perform linear algebraic operations
+on vectors and matricies, specifcally, but not limited to, common functions found
+in the study of robotics.
+
+This module depends heavily on the numpy package.
+
+"""
+
+import numpy as np
+
+
+def translate(vector, x, y):
+    """Return 2D vector translated by given x and y values
+
+    Parameters
+    ----------
+    vector : Iterable[int]
+        numpy ndarray of shape (2,)
+    x : int
+        The x translation.
+    y : int
+        The y translation.
+
+    Returns
+    ----------
+    Iterable[Int]
+        numpy ndarray of the x- and y-translation applied to the vector.
+
+    Raises
+    ----------
+    ValueError
+        If `vector` is not of shape `(2,)`
+
+    Examples
+    ----------
+    Translates x and y of origin vector:
+    >>> translate(np.array([0, 0]), 1, 1)
+    array([1, 1])
+
+    Translates x and y arbitrary vector:
+    >>> translate(np.array([1, 3]), 2, 2)
+    array([3, 5])
+
+    """
+    return np.array([x, y]) + vector
--- transform2d_tests.py
+++ transform2d_tests.py
@@ -1,10 +1,43 @@
 import unittest
+from numpy.testing import assert_, assert_raises
+import numpy as np
 import transform2d
 
 
 class TestTransform2d(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual(transform2d.foo(), 'bar')
+    def test_translate_by_0_will_return_original_vector(self):
+        vector = np.array([0, 0])
+        result = transform2d.translate(vector, 0, 0)
+        expected = vector
+        np.testing.assert_array_equal(result, expected)
+
+    def test_translate_by_1_in_x_will_return_translated_vector(self):
+        vector = np.array([0, 0])
+        result = transform2d.translate(vector, 1, 0)
+        expected = np.array([1, 0])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_translate_by_1_in_y_will_return_translated_vector(self):
+        vector = np.array([0, 0])
+        result = transform2d.translate(vector, 0, 1)
+        expected = np.array([0, 1])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_translate_by_1_x_and_1_y_will_return_translated_vector(self):
+        vector = np.array([0, 0])
+        result = transform2d.translate(vector, 1, 1)
+        expected = np.array([1, 1])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_translate_by_x_and_y_will_return_translated_vector(self):
+        vector = np.array([3, 1])
+        result = transform2d.translate(vector, 5, 8)
+        expected = np.array([8, 9])
+        np.testing.assert_array_equal(result, expected)
+
+    def test_translate_with_3d_vector_will_raise_exception(self):
+        vector = np.array([0, 0, 0])
+        self.assertRaises(ValueError, transform2d.translate, vector, 1, 1)
 
 
 if __name__ == '__main__':
