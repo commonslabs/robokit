1c9626c562ed738658775124b855e797b59dcdd0
Thomas Countz <thomascountz@gmail.com>
2018-11-20 14:51:01 -0500

    change transformations to return matrices

--- transform2d.py
+++ transform2d.py
@@ -11,80 +11,64 @@ This module depends heavily on the numpy package.
 import numpy as np
 
 
-def translate2d(vector, x, y):
-    """Return 2D vector translated by given x and y values
+def translate2d(x, y):
+    """Returns 2D translation matrix for given x and y
 
     Args:
-        vector (Iterable[int]): numpy ndarray of shape (2,)
         x (int): The x translation.
         y (int): The y translation.
 
     Returns:
-        Iterable[Int]: numpy ndarray of the x- and y-translation applied to the vector.
-
-    Raises:
-        ValueError: If `vector` is not broadcastable into `(2,)`
+        Iterable[Int]: numpy ndarray of the x- and y-translation.
 
     Examples:
-        Translates x and y of origin vector:
-        >>> translate2d(np.array([0, 0]), 1, 1)
-        array([1, 1])
+        Returns translation identity matrix for x and y:
+        >>> translate2d(0, 0)
+        array([[1, 0, 0],
+               [0, 1, 0],
+               [0, 0, 1]])
 
-        Translates x and y arbitrary vector:
-        >>> translate2d(np.array([1, 3]), 2, 2)
-        array([3, 5])
+        Returns translation matrix for x and y:
+        >>> translate2d(1, 1)
+        array([[1, 0, 1],
+               [0, 1, 1],
+               [0, 0, 1]])
 
-        Boadcasts when `vector` argument is a scalar:
-        >>> translate2d(1, 1, 1)
-        array([2, 2])
-
-        Boadcasts when translation is a boardast-able into vector shape:
-        >>> translate2d(np.array([[1, 1],[1, 1]]), 1, 1)
-        array([[2, 2],
-               [2, 2]])
+        Returns translation matrix for x and y:
+        >>> translate2d(2, 2)
+        array([[1, 0, 2],
+               [0, 1, 2],
+               [0, 0, 1]])
 
     """
-    return np.array([x, y]) + vector
+    return np.array([[1, 0, x], [0, 1, y], [0, 0, 1]])
 
 
-def rotate2d(vector, degrees):
-    """Return 2D vector translated by given angle in degrees
+def rotate2d(degrees):
+    """Return 2D rotation matrix based on the given angle
 
     Args:
-        vector (Iterable[int]): numpy ndarray of shape (2,)
         degrees (int): The angle of degrees to rotate
 
     Returns:
-        Iterable[Int]: numpy ndarray of a rotation matrix applied to the vector.
-
-    Raises:
-        ValueError: If `vector` is not broadcastable into `(2,)`
+        Iterable[Int]: numpy ndarray of a rotation matrix
 
     Examples:
-        Rotating the by 0 degrees will return the original vector:
-        >>> rotate2d(np.array([3, 8]), 0)
-        array([3., 8.])
+        Returns translation identity matrix for x and y:
+        >>> rotate2d(0)
+        array([[ 1., -0.],
+               [ 0.,  1.]])
 
         Rotating the origin will return the origin:
-        >>> rotate2d(np.array([0, 0]), 20)
-        array([0., 0.])
-
-        Rotates arbitrary vector:
-        >>> rotate2d(np.array([1, 0]), 20)
-        array([0.93969262, 0.34202014])
-
-        Boadcasts when `vector` argument is a scalar:
-        >>> rotate2d(1, 20)
+        >>> rotate2d(20)
         array([[ 0.93969262, -0.34202014],
                [ 0.34202014,  0.93969262]])
 
-        Boadcasts when vector is a boardast-able into (2,) shape:
-        >>> rotate2d(np.array([[1, 1],[1, 1]]), 20)
-        array([[0.59767248, 0.59767248],
-               [1.28171276, 1.28171276]])
-
+        Rotating the origin will return the origin:
+        >>> rotate2d(45)
+        array([[ 0.70710678, -0.70710678],
+               [ 0.70710678,  0.70710678]])
     """
     theta = np.radians(degrees)
-    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],
-                                [np.sin(theta), np.cos(theta)]])
-    return rotation_matrix.dot(vector)
+    return np.array([[np.cos(theta), -np.sin(theta)],
+                     [np.sin(theta), np.cos(theta)]])
--- transform2d_tests.py
+++ transform2d_tests.py
@@ -5,89 +5,41 @@ import transform2d
 
 
 class TestTransform2d(unittest.TestCase):
-    def test_translate2d_by_0_will_return_original_vector(self):
-        vector = np.array([0, 0])
-        result = transform2d.translate2d(vector, 0, 0)
-        expected = vector
+    def test_translate2d_returns_a_translation_identity_matrix(self):
+        result = transform2d.translate2d(0, 0)
+        expected = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_in_x_will_return_translate2d_vector(self):
-        vector = np.array([0, 0])
-        result = transform2d.translate2d(vector, 1, 0)
-        expected = np.array([1, 0])
+    def test_translate2d_returns_a_translation_matrix_in_x(self):
+        result = transform2d.translate2d(1, 0)
+        expected = np.array([[1, 0, 1], [0, 1, 0], [0, 0, 1]])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_in_y_will_return_translate2d_vector(self):
-        vector = np.array([0, 0])
-        result = transform2d.translate2d(vector, 0, 1)
-        expected = np.array([0, 1])
+    def test_translate2d_returns_a_translation_matrix_in_y(self):
+        result = transform2d.translate2d(0, 1)
+        expected = np.array([[1, 0, 0], [0, 1, 1], [0, 0, 1]])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_1_x_and_1_y_will_return_translate2d_vector(self):
-        vector = np.array([0, 0])
-        result = transform2d.translate2d(vector, 1, 1)
-        expected = np.array([1, 1])
+    def test_translate2d_returns_a_translation_matrix_in_x_and_y(self):
+        result = transform2d.translate2d(1, 1)
+        expected = np.array([[1, 0, 1], [0, 1, 1], [0, 0, 1]])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_by_x_and_y_will_return_translate2d_vector(self):
-        vector = np.array([3, 1])
-        result = transform2d.translate2d(vector, 5, 8)
-        expected = np.array([8, 9])
+    def test_translate2d_returns_a_translation_matrix(self):
+        result = transform2d.translate2d(5, 8)
+        expected = np.array([[1, 0, 5], [0, 1, 8], [0, 0, 1]])
         np.testing.assert_array_equal(result, expected)
 
-    def test_translate2d_with_scalar_will_boardcast(self):
-        scalar = 1
-        result = transform2d.translate2d(scalar, 1, 1)
-        expected = np.array([2, 2])
-        np.testing.assert_array_equal(result, expected)
-
-    def test_translate2d_with_broadcastable_vector_will_boardcast(self):
-        boardcastable_vector = np.array([[1, 1], [1, 1]])
-        result = transform2d.translate2d(boardcastable_vector, 1, 1)
-        expected = np.array([[2, 2], [2, 2]])
-
-    def test_translate2d_with_nonbroadcastable_vector_will_raise_exception(
-            self):
-        vector = np.array([0, 0, 0])
-        self.assertRaises(ValueError, transform2d.translate2d, vector, 1, 1)
-
-    def test_rotate2d_by_0_degrees_will_return_original_vector(self):
-        vector = np.array([0, 0])
-        result = transform2d.rotate2d(vector, 0)
-        expected = np.array([0, 0])
-        np.testing.assert_array_equal(result, expected)
-
-    def test_rotate2d_origin_vectors_rotated_will_return_an_origin_vector(
-            self):
-        vector = np.array([0, 0])
-        result = transform2d.rotate2d(vector, 20)
-        expected = np.array([0, 0])
-        np.testing.assert_array_equal(result, expected)
-
-    def test_rotated2d_vector_rotated_by_20_degrees_will_return_rotated_vector(
-            self):
-        vector = np.array([1, 0])
-        result = transform2d.rotate2d(vector, 20)
-        expected = np.array([0.93969262, 0.34202014])
+    def test_rotate2d_returns_a_rotation_identity_matrix(self):
+        result = transform2d.rotate2d(0)
+        expected = np.array([[1, 0], [0, 1]])
         np.testing.assert_array_almost_equal(result, expected)
 
-    def test_rotate2d_with_scalar_will_boardcast(self):
-        scalar = 1
-        result = transform2d.rotate2d(scalar, 20)
+    def test_translate2d_returns_a_rotation_matrix(self):
+        result = transform2d.rotate2d(20)
         expected = np.array([[0.93969262, -0.34202014],
                              [0.34202014, 0.93969262]])
         np.testing.assert_array_almost_equal(result, expected)
-
-    def test_rotate2d_with_broadcastable_vector_will_boardcast(self):
-        boardcastable_vector = np.array([[1, 1], [1, 1]])
-        result = transform2d.rotate2d(boardcastable_vector, 20)
-        expected = np.array([[0.59767248, 0.59767248],
-                             [1.28171276, 1.28171276]])
-        np.testing.assert_array_almost_equal(result, expected)
-
-    def test_rotate2d_with_nonbroadcastable_vector_will_raise_exception(self):
-        vector = np.array([0, 0, 0])
-        self.assertRaises(ValueError, transform2d.rotate2d, vector, 20)
 
 
 if __name__ == '__main__':
