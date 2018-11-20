061c1598be4affeabc5e9bbe1c527d328df268a8
Thomas Countz <thomascountz@gmail.com>
2018-11-20 08:39:18 -0500

    use google style python docstrings

--- transform2d.py
+++ transform2d.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """transform2d
 
-This module contains several funtions to perform linear algebraic operations
-on vectors and matricies, specifcally, but not limited to, common functions found
-in the study of robotics.
+This module contains several funtions to perform 2D operations on vectors and matricies,
+specifcally, but not limited to, common functions found in the study of robotics.
 
 This module depends heavily on the numpy package.
 
@@ -15,34 +14,25 @@ import numpy as np
 def translate(vector, x, y):
     """Return 2D vector translated by given x and y values
 
-    Parameters
-    ----------
-    vector : Iterable[int]
-        numpy ndarray of shape (2,)
-    x : int
-        The x translation.
-    y : int
-        The y translation.
+    Args:
+        vector (Iterable[int]): numpy ndarray of shape (2,)
+        x (int): The x translation.
+        y (int): The y translation.
 
-    Returns
-    ----------
-    Iterable[Int]
-        numpy ndarray of the x- and y-translation applied to the vector.
+    Returns:
+        Iterable[Int]: numpy ndarray of the x- and y-translation applied to the vector.
 
-    Raises
-    ----------
-    ValueError
-        If `vector` is not of shape `(2,)`
+    Raises:
+        ValueError: If `vector` is not of shape `(2,)`
 
-    Examples
-    ----------
-    Translates x and y of origin vector:
-    >>> translate(np.array([0, 0]), 1, 1)
-    array([1, 1])
+    Examples:
+        Translates x and y of origin vector:
+        >>> translate(np.array([0, 0]), 1, 1)
+        array([1, 1])
 
-    Translates x and y arbitrary vector:
-    >>> translate(np.array([1, 3]), 2, 2)
-    array([3, 5])
+        Translates x and y arbitrary vector:
+        >>> translate(np.array([1, 3]), 2, 2)
+        array([3, 5])
 
     """
     return np.array([x, y]) + vector
