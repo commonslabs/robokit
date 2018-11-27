20eb45848a7b879fc1ef0b7940bc0b90c42b68f5
Thomas Countz <thomascountz@gmail.com>
2018-11-26 21:09:15 -0500

    add push.sh

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git config --global user.email "travis@travis-ci.org"
+git confit --global user.name "Travis CI"
+
+git checkout -b pre-deploy
+
+python3 py2md.py --sourcedir ./robokit/ --docfile docs.md -projectname "Robokit" --codelinks
+
+git commit --message "Travis Build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin https://github.com/Thomascountz/robokit.git
+
+git push --setup-upstream origin pre-deploy
