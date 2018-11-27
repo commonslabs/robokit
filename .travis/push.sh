34c7d1dfbb99a372b3f42c124b33a0e128796319
Thomas Countz <thomascountz@gmail.com>
2018-11-26 21:14:40 -0500

    add token to travis

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,6 +9,6 @@ python3 py2md.py --sourcedir ./robokit/ --docfile docs.md -projectname "Robokit"
 
 git commit --message "Travis Build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin https://github.com/Thomascountz/robokit.git
+git remote add origin https://${GH_TOKEN}@github.com/Thomascountz/robokit.git
 
 git push --setup-upstream origin pre-deploy
